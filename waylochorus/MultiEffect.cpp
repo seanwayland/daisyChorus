a20ebe17ac57d27139f9773ae060b040f55efd41
seanwayland <seanwayland@gmail.com>
2022-04-09 21:37:16 -0400

    broken !

--- waylochorus/MultiEffect.cpp
+++ waylochorus/MultiEffect.cpp
@@ -8,6 +8,9 @@
 #define COR 2
 #define PHR 3
 #define OCT 4
+#define WAYLOCHORUS 5
+#define SR 48000
+#define M_PI   3.14159265358979323846  /* pi */
 
 
 using namespace daisysp;
@@ -36,6 +39,35 @@ uint32_t octDelSize;
 
 float currentDelay, feedback, delayTarget, freq, freqtarget, lfotarget, lfo;
 
+int mCircularBufferWriteHead =0;
+int mCircularBufferWriteHeadTwo =0;
+int mCircularBufferWriteHeadThree =0;
+int mCircularBufferWriteHeadFour = 0;
+int mDelayReadHead = 0;
+int mDelayTwoReadHead = 0;
+int mDelayThreeReadHead = 0;
+int mDelayFourReadHead =0;
+
+float mRateParameter = 1.0;
+
+
+float mLFOphase = 0;
+float mLFOphaseTwo= 0;
+float mLFOphaseThree= 0;
+float mLFOphaseFour= 0;
+
+float *mCircularBufferLeft= new float[SR];
+float *mCircularBufferRight= new float[SR];
+float *mCircularBufferLeftTwo =new float[SR];
+float *mCircularBufferRightTwo = new float[SR];
+float *mCircularBufferLeftThree =new float[SR];
+float *mCircularBufferRightThree= new float[SR];
+float *mCircularBufferLeftFour = new float[SR];
+float *mCircularBufferRightFour =new float[SR];
+
+
+
+
 
 float  drywet;
 
@@ -52,6 +84,15 @@ void GetChorusSample(float &outl, float &outr, float inl, float inr);
 void GetPhaserSample(float &outl, float &outr, float inl, float inr);
 
 void GetOctaveSample(float &outl, float &outr, float inl, float inr);
+
+void GetWayloChorusSample(float &outl, float &outr, float inl, float inr);
+
+
+float linInterp(float sample_x, float sample_x1, float inPhase)
+{
+    return (1 - inPhase) * sample_x + inPhase * sample_x1;
+    
+}
 
 
 void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
@@ -75,6 +116,7 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
             case COR: GetChorusSample(outl, outr, inl, inr); break;
             case PHR: GetPhaserSample(outl, outr, inl, inr); break;
             case OCT: GetOctaveSample(outl, outr, inl, inr); break;
+            case WAYLOCHORUS: GetWayloChorusSample(outl, outr, inl, inr); break;\
             default: outl = outr = 0;
         }
 
@@ -89,6 +131,22 @@ int main(void)
 {
     // initialize pod hardware and oscillator daisysp module
     float sample_rate;
+
+    for (int i = 0; i < SR -1; i++) {
+
+        mCircularBufferLeft[i]= 0.0f;
+        mCircularBufferRight[i]= 0.0f;
+        mCircularBufferLeftTwo[i]= 0.0f;
+        mCircularBufferRightTwo[i]= 0.0f;
+        mCircularBufferLeftThree[i]= 0.0f;
+        mCircularBufferRightThree[i]= 0.0f;
+        mCircularBufferLeftFour[i]= 0.0f;
+        mCircularBufferRightFour[i]= 0.0f;
+
+}
+
+
+    
 
     //Inits and sample rate
     pod.Init();
@@ -227,8 +285,11 @@ void UpdateKnobs(float &k1, float &k2)
             drywet = k1;
             break;
 
-
-
+        case WAYLOCHORUS:
+            drywet = k1;
+            mRateParameter = k2;
+            break;
+           
            
     }
 }
@@ -236,7 +297,7 @@ void UpdateKnobs(float &k1, float &k2)
 void UpdateEncoder()
 {
     mode = mode + pod.encoder.Increment();
-    mode = (mode % 5 + 5) % 5;
+    mode = (mode % 6 + 6) % 6;
 }
 
 void UpdateLeds(float k1, float k2)
@@ -361,4 +422,223 @@ void GetPhaserSample(float &outl, float &outr, float inl, float inr){
 
 
             }
+
+
+void GetWayloChorusSample(float &outl, float &outr, float inl, float inr){
+
+        float mDelayOneModRateParameter = 0.35f;
+        float mDelayOneModRateParameterTwo = 0.2f;
+        float mDelayOneModRateParameterThree = 0.4f;
+        float mDelayOneModRateParameterFour = 0.5f;
+
+
+        mLFOphase += mDelayOneModRateParameter * mRateParameter / SR;
+
+        if ( mLFOphase > 1){
+            mLFOphase -= 1;
+        }
+
+        float lfoOut = sin(2*M_PI * mLFOphase);
+
+        
+        mLFOphaseTwo += mDelayOneModRateParameterTwo * mRateParameter / SR;
+
+        if ( mLFOphaseTwo > 1){
+            mLFOphaseTwo -= 1;
+        }
+
+        float lfoOutTwo = sin(2*M_PI * mLFOphaseTwo);
+
+        mLFOphaseThree += mDelayOneModRateParameterThree * mRateParameter / SR;
+
+        if ( mLFOphaseThree > 1){
+            mLFOphaseThree -= 1;
+        }
+
+        float lfoOutThree = sin(2*M_PI * mLFOphaseThree);
+
+        
+        mLFOphaseFour += mDelayOneModRateParameterFour * mRateParameter / SR;
+
+        if ( mLFOphaseFour > 1){
+            mLFOphaseFour -= 1;
+        }
+
+        float lfoOutFour = sin(2*M_PI * mLFOphaseFour);
+
+        // map values from -1, 1  to .001 , .1 
+        float lfoOutMapped = 0.001f + ((0.099f) * (lfoOut +1.f)) / (2.f);
+        float lfoOutMappedTwo = 0.001f + ((0.099f) * (lfoOutTwo +1.f)) / (2.f);
+        float lfoOutMappedThree = 0.001f + ((0.099f) * (lfoOutThree +1.f)) / (2.f);
+        float lfoOutMappedFour = 0.001f + ((0.099f) * (lfoOutFour +1.f)) / (2.f);
+
+
+        float mDelayOneTimeParameter = 0.0236f;
+        float mDelayTwoTimeParameter = 0.030f;
+        float mDelayThreeTimeParameter = 0.0361f;
+        float mDelayFourTimeParameter = 0.0476f;
+
+        int dtime = static_cast<int>(mDelayOneTimeParameter*SR);
+        int dtimeTwo = static_cast<int>(mDelayTwoTimeParameter*SR);
+        int dtimeThree = static_cast<int>(mDelayThreeTimeParameter*SR);
+        int dtimeFour = static_cast<int>(mDelayFourTimeParameter*SR);
+
+        int mDelayTimeInSamples = dtime*(1+ lfoOutMapped) ;
+        int mDelayTwoTimeInSamples = dtimeTwo*(1+ lfoOutMappedTwo);
+        int mDelayThreeTimeInSamples = dtimeThree*(1+ lfoOutMappedThree);
+        int mDelayFourTimeInSamples = dtimeFour*(1+ lfoOutMappedFour) ;
+
+
+        //// shove some of the input into the circular buffer also add some of the feedback
+        mCircularBufferLeft[mCircularBufferWriteHead] = inl;
+        mCircularBufferRight[mCircularBufferWriteHead] = inr;
+
+        mCircularBufferLeftTwo[mCircularBufferWriteHead] = inl;
+        mCircularBufferRightTwo[mCircularBufferWriteHead] = inr;
+
+        mCircularBufferLeftThree[mCircularBufferWriteHead] = inl;
+        mCircularBufferRightThree[mCircularBufferWriteHead] = inr;
+
+        mCircularBufferLeftFour[mCircularBufferWriteHead] = inl;
+        mCircularBufferRightFour[mCircularBufferWriteHead] = inr;
+
+
+                // move the read head on the circular to the new delay position
+        mDelayReadHead = mCircularBufferWriteHead - mDelayTimeInSamples;
+        mDelayTwoReadHead = mCircularBufferWriteHeadTwo - mDelayTwoTimeInSamples;
+        mDelayThreeReadHead = mCircularBufferWriteHeadThree - mDelayThreeTimeInSamples;
+        mDelayFourReadHead = mCircularBufferWriteHeadFour - mDelayFourTimeInSamples;
+
+                // if read head is below zero wrap around
+        if (mDelayReadHead < 0){
+            mDelayReadHead += SR;
+        }
+        
+        if (mDelayTwoReadHead < 0){
+            mDelayTwoReadHead += SR;
+        }
+        if (mDelayThreeReadHead < 0){
+            mDelayThreeReadHead += SR;
+        }
+        if (mDelayFourReadHead < 0){
+            mDelayFourReadHead += SR;
+        }
+
+        // get the integer part of the read head
+        int readHeadX = (int)mDelayReadHead;
+        // get the part of the readHead after the decimal point
+        float readHeadFloat = mDelayReadHead - readHeadX;
+        // next integer sample position
+        int readHeadX1 = readHeadX + 1;
+        
+        // get the integer part of the read head
+        int readHeadXTwo = (int)mDelayTwoReadHead;
+        // get the part of the readHead after the decimal point
+        float readHeadFloatTwo = mDelayTwoReadHead - readHeadXTwo;
+        // next integer sample position
+        int readHeadX1Two = readHeadXTwo + 1;
+        
+        // get the integer part of the read head
+        int readHeadXThree = (int)mDelayThreeReadHead;
+        // get the part of the readHead after the decimal point
+        float readHeadFloatThree = mDelayThreeReadHead - readHeadXThree;
+        // next integer sample position
+        int readHeadX1Three = readHeadXThree + 1;
+        
+        // get the integer part of the read head
+        int readHeadXFour = (int)mDelayFourReadHead;
+        // get the part of the readHead after the decimal point
+        float readHeadFloatFour = mDelayFourReadHead - readHeadXFour;
+        // next integer sample position
+        int readHeadX1Four = readHeadXFour + 1;
+
+
+
+
+                // get the interpolated value of the delayed sample from the circular buffer
+        float delay_sample_Left = linInterp(mCircularBufferLeft[readHeadX], mCircularBufferLeft[readHeadX1], readHeadFloat);
+        float delay_sample_Right = linInterp(mCircularBufferRight[readHeadX], mCircularBufferRight[readHeadX1], readHeadFloat);
+        float delay_sample_LeftTwo = linInterp(mCircularBufferLeftTwo[readHeadXTwo], mCircularBufferLeftTwo[readHeadX1Two], readHeadFloatTwo);
+        float delay_sample_RightTwo = linInterp(mCircularBufferRightTwo[readHeadXTwo], mCircularBufferRightTwo[readHeadX1Two], readHeadFloatTwo);
+        float delay_sample_LeftThree = linInterp(mCircularBufferLeftThree[readHeadXThree], mCircularBufferLeftThree[readHeadX1Three], readHeadFloatThree);
+        float delay_sample_RightThree = linInterp(mCircularBufferRightThree[readHeadXThree], mCircularBufferRightThree[readHeadX1Three], readHeadFloatThree);
+        float delay_sample_LeftFour = linInterp(mCircularBufferLeftFour[readHeadXFour], mCircularBufferLeftFour[readHeadX1Four], readHeadFloatFour);
+        float delay_sample_RightFour = linInterp(mCircularBufferRightFour[readHeadXFour], mCircularBufferRightFour[readHeadX1Four], readHeadFloatFour);
+
+        float mDelayOneGainParameter = 0.2f;
+        float mDelayTwoGainParameter = 0.4f;
+        float mDelayThreeGainParameter = 0.4f;
+        float mDelayFourGainParameter = 0.4f;
+
+        outl = inl*0.4f + delay_sample_Left*0.4f
+
+                         
+                         /***
+                          + delay_sample_Left* *mDelayOneGainParameter* *drywet + delay_sample_Right* *mDelayOneGainParameter* *drywet
+                          +delay_sample_LeftThree* *mDelayThreeGainParameter* *drywet+ delay_sample_RightThree* *mDelayThreeGainParameter* *drywet
+                          +delay_sample_LeftFive* *mDelayFiveGainParameter* *drywet+ delay_sample_RightFive* *mDelayFiveGainParameter* *drywet
+                          +delay_sample_LeftSeven* *mDelaySevenGainParameter* *drywet+ delay_sample_RightSeven* *mDelaySevenGainParameter* *drywet
+                          ***/
+                         
+                       //  + delay_sample_Left*mDelayOneGainParameter + delay_sample_Right*mDelayOneGainParameter
+                        // +delay_sample_LeftThree*mDelayThreeGainParameter+ delay_sample_RightThree*mDelayThreeGainParameter*drywet
+                    
+                        // + delay_sample_LeftTwo*mDelayTwoGainParameter*drywet+ delay_sample_RightTwo*mDelayTwoGainParameter*drywet
+                        // + delay_sample_LeftFour*mDelayFourGainParameter*drywet+ delay_sample_RightFour*mDelayFourGainParameter*drywet
+
+                         
+                         ;
+        
+        outr = (inr*drywet
+                         
+                         
+                         
+                         /***
+                          + delay_sample_LeftTwo* *mDelayTwoGainParameter* *drywet+ delay_sample_RightTwo* *mDelayTwoGainParameter* *drywet
+                          + delay_sample_LeftFour* *mDelayFourGainParameter* *drywet+ delay_sample_RightFour* *mDelayFourGainParameter* *drywet
+                          + delay_sample_LeftSix* *mDelaySixGainParameter* *drywet+ delay_sample_RightSix* *mDelaySixGainParameter* *drywet
+                          + delay_sample_LeftEight* *mDelayEightGainParameter* *drywet+ delay_sample_RightEight* *mDelayEightGainParameter* *drywet
+                          ***/
+                         
+                         + delay_sample_Left*mDelayOneGainParameter* drywet + delay_sample_Right*mDelayOneGainParameter*drywet
+                         +delay_sample_LeftThree*mDelayThreeGainParameter* drywet+ delay_sample_RightThree*mDelayThreeGainParameter*drywet
+                         + delay_sample_LeftTwo*mDelayTwoGainParameter* drywet+ delay_sample_RightTwo*mDelayTwoGainParameter*drywet
+                         + delay_sample_LeftFour*mDelayFourGainParameter* drywet+ delay_sample_RightFour*mDelayFourGainParameter*drywet
+
+                         
+                         
+                         );
+        
+
+
+        outl = inl * (1.f - lfoOutMapped*10);
+        outr =  inr * (1.f - lfoOutMappedTwo*10);
+
+                //         // increment the buffer write head
+        mCircularBufferWriteHead ++;
+        mCircularBufferWriteHeadTwo ++;
+        mCircularBufferWriteHeadThree ++;
+        mCircularBufferWriteHeadFour ++;
+
+        
+        // wrap around if needed
+        if (mCircularBufferWriteHead == 5000){
+            mCircularBufferWriteHead = 0;
+        }
+        if (mCircularBufferWriteHeadTwo == SR){
+            mCircularBufferWriteHeadTwo = 0;
+        }
+        if (mCircularBufferWriteHeadThree == SR){
+            mCircularBufferWriteHeadThree = 0;
+        }
+        if (mCircularBufferWriteHeadFour == SR){
+            mCircularBufferWriteHeadFour = 0;
+        }
+
+    
+            //outl =  psr.Process(inl * drywet) + inl*(1.0f-drywet);
+           // outr =  psr.Process(inr * drywet) + inr*((1.0f-drywet));
+            
+}
+
 
